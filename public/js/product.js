cbfb796fd610872546052aa49c22696c7c864a0e
Tomiwa_Jegede <jegedetomiwa1@gmail.com>
2024-08-14 00:59:27 +0100

    add more product to the page

--- public/js/product.js
+++ public/js/product.js
@@ -1,53 +1,93 @@
 export const products = [
   {
-    video:"https://drive.google.com/file/d/1Bhqo7vQunIkrHCI6wC7uTymrOBefuQ1m/preview",
+    video: "https://drive.google.com/file/d/1Bhqo7vQunIkrHCI6wC7uTymrOBefuQ1m/preview",
     name: "American Eskimo",
     age: "8 Weeks",
     sex: "Both",
     location: "Lagos( Ikotun)",
     price: "70,000",
     id: "PET-DOG-001-AE",
-    about:"https://youtu.be/9dE5kNIt0ss?si=L6aqz2DbiJOUU94r"
+    about: "https://youtu.be/9dE5kNIt0ss?si=L6aqz2DbiJOUU94r"
   },
   {
-    video:"https://drive.google.com/file/d/1sOduRv_YhKaXmun8BDhUF_lT439OvYrY/preview",
+    video: "https://drive.google.com/file/d/1sOduRv_YhKaXmun8BDhUF_lT439OvYrY/preview",
     name: "Lhasa Apso",
     age: "8 Weeks",
     sex: "Male",
     location: "Lagos( Ikeja)",
     price: "80,000",
     id: "PET-DOG-001-LS",
-    about:"https://youtu.be/8Qx2hxI29Vk?si=XL7_9VJhlTb866qe"
+    about: "https://youtu.be/8Qx2hxI29Vk?si=XL7_9VJhlTb866qe"
   },
   {
-    video:"https://drive.google.com/file/d/10JGKnWKWclwDGLUJqao1XjiqHCmIitWf/preview",
+    video: "https://drive.google.com/file/d/10JGKnWKWclwDGLUJqao1XjiqHCmIitWf/preview",
     name: "Pitbull",
     age: "6 Weeks",
     sex: "Male",
     location: "Lagos( Ikeja)",
     price: "165,000",
     id: "PET-DOG-001-PIT",
-    about:"https://youtu.be/cytcIA08o-E?si=g7M3wE_tglv56TJJ"
+    about: "https://youtu.be/cytcIA08o-E?si=g7M3wE_tglv56TJJ"
   },
   {
-    video:"https://drive.google.com/file/d/1p9CoiKJorD-QT6FKM7tHpiAZcWl5X2qb/preview",
+    video: "https://drive.google.com/file/d/1p9CoiKJorD-QT6FKM7tHpiAZcWl5X2qb/preview",
     name: "Cane Corso",
     age: "6 Weeks",
     sex: "Both",
     location: "Abuja(lokogoma axis)",
     price: "400,000",
     id: "SECURITY-DOG-001-CC",
-    about:"https://youtu.be/lWT2ER5gJWA?si=CiDWoHgB6BUEJon2"
+    about: "https://youtu.be/lWT2ER5gJWA?si=CiDWoHgB6BUEJon2"
   },
   {
-    video:"https://drive.google.com/file/d/1kII2S5fztKtAKtfCBg0u_Raf0D0wxzX1/preview",
+    video: "https://drive.google.com/file/d/1kII2S5fztKtAKtfCBg0u_Raf0D0wxzX1/preview",
     name: "Cocker Spaniel",
     age: "1 Year 7 Months",
     sex: "Female",
     location: "Lagos()",
     price: "1,100,000",
     id: "PET-DOG-001-CS",
-    about:"https://youtu.be/b_un030JwKA?si=n5yBUy83UIwSotvO"
+    about: "https://youtu.be/b_un030JwKA?si=n5yBUy83UIwSotvO"
+  },
+  {
+    video: "https://drive.google.com/file/d/1eq_pV_gAMU-pwpVM0ykxYj3bhHm8wgIg/preview",
+    name: "BoerBoel",
+    age: "8 Weeks",
+    sex: "Male",
+    location: "Ogun(sango ota)",
+    price: "120,000",
+    id: "SECURITY-DOG-001-BB",
+    about: "https://youtu.be/kBxFOvz7Xao?si=KImxUS3ZkGrHyL7p"
+  },
+  {
+    video: "https://drive.google.com/file/d/1DZD2gTiQHvfgiVReDu5o8EmAAoXHdOGP/preview",
+    name: "Lhasa Apso",
+    age: "6 Weeks ",
+    sex: "Male",
+    location: "lagos(Ikotun)",
+    price: "65,000",
+    id: "PET-DOG-002-LS",
+    about: "https://youtu.be/8Qx2hxI29Vk?si=XL7_9VJhlTb866qe"
+  },
+  {
+    video: "https://drive.google.com/file/d/1ae_glo1JVmHImLxUA9cT8lw7Tnz5A2tL/preview",
+    name: "American Eskimo",
+    age: "1 Year 4 Months",
+    sex: "Male",
+    location: "lagos(Ikotun)",
+    price: "120,000",
+    id: "PET-DOG-002-AE",
+    about: "https://youtu.be/9dE5kNIt0ss?si=L6aqz2DbiJOUU94r"
+  },
+  {
+    video: "https://drive.google.com/file/d/1Pu7NzWjDS4k0Yz4CmsAihcPUvlJ-OhGr/preview",
+    name: "German Sherpherd",
+    age: "6 weeks",
+    sex: "Male",
+    location: "lagos(Ikotun)",
+    price: "60,000",
+    id: "SECURITY-DOG-001-GSD",
+    about: "https://youtu.be/p0INBNLqmPE?si=Y_mhqG0uAbnhCeHg"
   },
 
 
