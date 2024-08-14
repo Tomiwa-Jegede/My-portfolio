5489206a378bdaa63ed0c36c1d92516f6ba55f5b
Tomiwa_Jegede <jegedetomiwa1@gmail.com>
2024-08-14 22:40:31 +0100

    .

--- public/js/generationofHTML/productHTML.js
+++ public/js/generationofHTML/productHTML.js
@@ -3,36 +3,36 @@ export let product = document.getElementById("product-container");
 export let productHTML = "";
 products.forEach((product) => {
   productHTML +=
-    `<div id="product" class="w-80">
-    <div class="display my-4 shadow-md w-80 rounded-lg">
-        <iframe class="rounded-lg shadow-md" src="https://drive.google.com/file/d/1Bhqo7vQunIkrHCI6wC7uTymrOBefuQ1m/preview" width="100%" allowfullscreen=""></iframe>
-    </div>
+  `<div id="product" class="w-80">
+  <div class="display my-4 shadow-md w-80 rounded-lg">
+      <iframe class="rounded-lg shadow-md" src="${product.video}" width="100%" allowfullscreen=""></iframe>
+  </div>
 
-    <div>
-        <div class="mb-2">
-            <span class="font-bold">Name:</span> <a href="https://youtu.be/9dE5kNIt0ss?si=L6aqz2DbiJOUU94r" target="blank"><span class="transiton duration-200 ease-in-out hover:text-orange-500 hover:font-bold">American Eskimo</span></a>
-        </div>
+  <div>
+      <div class="mb-2">
+          <span class="font-bold">Name:</span> <a href="${product.about}" target="blank"><span class="transiton duration-200 ease-in-out hover:text-orange-500 hover:font-bold">${product.name}</span></a>
+      </div>
 
-        <div class="mb-2"><span class="font-bold">Location:</span> <span>Lagos( Ikotun)</span></div>
-        <div class="mb-2"><span class="font-bold">Age:</span> <span>8 Weeks</span></div>
-        <div class="mb-2"><span class="font-bold">Sex:</span> <span>Both</span></div>
-        <span class="font-bold mb-4">Vaccinated</span>
-    </div>
-    <div class="mt-4 mb-4"><span class="font-bold">Price:</span> <span>₦70,000</span></div>
-    <div class="mb-4"><span class="font-bold">Dog ID:</span> <span>PET-DOG-001-AE</span></div>
-    <div class="leading-5 font-bold text-red-600">
-        <span class="fa-solid fa-bullhorn"></span>
-        <span>Attention Pet Owners!</span>
-        <p class="fa-solid fa-bullhorn"></p>
-        <p>You Can Click The Dogs Name For More Info About Breed. Send The Dog ID To The Number When You Click Adopt</p>
-    </div>
-    <div class="flex items-center justify-center">
-        <a href="https://wa.me/message/WUS7G3SB7NX5J1" target="blank" class="w-80">
-            <button class="rounded-full w-80 text-white bg-orange-500 shadow-md shadow-gray-900 p-2 mt-3 hover:text-orange-500 hover:bg-white hover:border border-orange-500 transition duration-200 ease-linear">
-                Adopt <span class="fa-solid fa-bone"></span>
-            </button>
-        </a>
-    </div>
+      <div class="mb-2"><span class="font-bold">Location:</span> <span>${product.location}</span></div>
+      <div class="mb-2"><span class="font-bold">Age:</span> <span>${product.age}</span></div>
+      <div class="mb-2"><span class="font-bold">Sex:</span> <span>${product.sex}</span></div>
+      <span class="font-bold mb-4">Vaccinated</span>
+  </div>
+  <div class="mt-4 mb-4"><span class="font-bold">Price:</span> <span>${product.price}</span></div>
+  <div class="mb-4"><span class="font-bold">Dog ID:</span> <span>${product.id}</span></div>
+  <div class="leading-5 font-bold text-red-600">
+      <span class="fa-solid fa-bullhorn"></span>
+      <span>Attention Pet Owners!</span>
+      <p class="fa-solid fa-bullhorn"></p>
+      <p>You Can Click The Dogs Name For More Info About Breed. Send The Dog ID To The Number When You Click Adopt</p>
+  </div>
+  <div class="flex items-center justify-center">
+      <a href="https://wa.me/message/WUS7G3SB7NX5J1" target="blank" class="w-80">
+          <button class="rounded-full w-80 text-white bg-orange-500 shadow-md shadow-gray-900 p-2 mt-3 hover:text-orange-500 hover:bg-white hover:border border-orange-500 transition duration-200 ease-linear">
+              Adopt <span class="fa-solid fa-bone"></span>
+          </button>
+      </a>
+   </div>
 </div>`;
 });
 export function generatedHTMLForTheProducts() {
