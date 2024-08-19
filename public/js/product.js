cce413cd1ede5806fdc1e47eed65877cf0d5448a
Tomiwa-Jegede <jegedetomiwa1@gmail.com>
2024-08-19 18:48:54 +0100

    updated the product that where on the page

--- public/js/product.js
+++ public/js/product.js
@@ -3,7 +3,7 @@ export const products = [
     video: "https://drive.google.com/file/d/1Bhqo7vQunIkrHCI6wC7uTymrOBefuQ1m/preview",
     name: "American Eskimo",
     age: "8 Weeks",
-    sex: "Both",
+    sex: "Male",
     location: "Lagos( Ikotun)",
     price: "70,000",
     id: "PET-DOG-001-AE",
@@ -60,16 +60,6 @@ export const products = [
     about: "https://youtu.be/kBxFOvz7Xao?si=KImxUS3ZkGrHyL7p"
   },
   {
-    video: "https://drive.google.com/file/d/1DZD2gTiQHvfgiVReDu5o8EmAAoXHdOGP/preview",
-    name: "Lhasa Apso",
-    age: "6 Weeks ",
-    sex: "Male",
-    location: "lagos(Ikotun)",
-    price: "65,000",
-    id: "PET-DOG-002-LS",
-    about: "https://youtu.be/8Qx2hxI29Vk?si=XL7_9VJhlTb866qe"
-  },
-  {
     video: "https://drive.google.com/file/d/1ae_glo1JVmHImLxUA9cT8lw7Tnz5A2tL/preview",
     name: "American Eskimo",
     age: "1 Year 4 Months",
@@ -80,20 +70,10 @@ export const products = [
     about: "https://youtu.be/9dE5kNIt0ss?si=L6aqz2DbiJOUU94r"
   },
   {
-    video: "https://drive.google.com/file/d/1Pu7NzWjDS4k0Yz4CmsAihcPUvlJ-OhGr/preview",
-    name: "German Sherpherd",
-    age: "6 weeks",
-    sex: "Male",
-    location: "lagos(Ikotun)",
-    price: "60,000",
-    id: "SECURITY-DOG-001-GSD",
-    about: "https://youtu.be/p0INBNLqmPE?si=Y_mhqG0uAbnhCeHg"
-  },
-  {
     video: "https://drive.google.com/file/d/1R54LLARQHXAaB9dzggCGAwK0MafZMvPO/preview",
     name: "Chow Chow",
     age: "7 weeks",
-    sex: "Both",
+    sex: "Male",
     location: "Akure",
     price: "500,000",
     id: "PET-DOG-001-CHOW",
@@ -129,6 +109,104 @@ export const products = [
     id: "PET-DOG-001-FH",
     about: "https://youtu.be/8lyPyFhpaYU?si=zAdIPzqOCjxjYixE"
   },
-
-
+  {
+    video: "https://drive.google.com/file/d/1Wnh847t4otuW2TIBCwT-inSVfGKorXex/preview",
+    name: "American Eskimo",
+    age: "8 Weeks",
+    sex: "Female",
+    location: "Lagos",
+    price: "75,000",
+    id: "PET-DOG-001-AE",
+    about: "https://youtu.be/9dE5kNIt0ss?si=L6aqz2DbiJOUU94r"
+  },
+  {
+    video: "https://drive.google.com/file/d/161AvimRHKzLTTguKqv6n-5CbW7kXW_ot/preview",
+    name: "Dobermann",
+    age: "4 months",
+    sex: "Female",
+    location: "Lagos",
+    price: "175,000",
+    id: "SECURITY-DOG-001-DB",
+    about: "https://youtu.be/PaCWHpmvbCc?si=1C2IwzptWvjH71I5"
+  },
+  {
+    video: "https://drive.google.com/file/d/1DZD2gTiQHvfgiVReDu5o8EmAAoXHdOGP/preview",
+    name: "Lhasa Apso",
+    age: "6 weeks",
+    sex: "Both",
+    location: "Ibadan",
+    price: "85,000/80,000",
+    id: "PET-DOG-002-LS",
+    about: ""
+  },
+  {
+    video: "https://drive.google.com/file/d/11wCCFco8MXMB39F2zEFcLlpFgc73IsSt/preview",
+    name: "RottWiller",
+    age: "7 weeks",
+    sex: "Female",
+    location: "Ibadan",
+    price: "170,000",
+    id: "SECURITY-DOG-001-RW",
+    about: "https://youtu.be/HPHKB5TXhPM?si=mqxKTFRYjMWt6cFM"
+  },
+  {
+    video: "https://drive.google.com/file/d/1SY-Oh35KDXwbB33HUMDjITwUFojPyzMt/preview",
+    name: "Cane Corso",
+    age: "6 weeks",
+    sex: "Both",
+    location: "Ibadan",
+    price: "350,000",
+    id: "SECURITY-DOG-002-CC",
+    about: "https://youtu.be/p0INBNLqmPE?si=Y_mhqG0uAbnhCeHg"
+  },
+  {
+    video: "https://drive.google.com/file/d/14FKWpZaKA_xOLqtMABDhnhHHmVZuIsI-/preview",
+    name: "German Sherpherd",
+    age: "6 weeks",
+    sex: "Male",
+    location: "Ogun",
+    price: "480,000",
+    id: "SECURITY-DOG-002-GSD",
+    about: "https://youtu.be/p0INBNLqmPE?si=Y_mhqG0uAbnhCeHg"
+  },
+  {
+    video: "https://drive.google.com/file/d/152SwfYXJeBq1_rwEMWEH4ogJWQ4Ol-J4/preview",
+    name: "German Sherpherd",
+    age: "7 weeks",
+    sex: "Female",
+    location: "lagos",
+    price: "400,000",
+    id: "SECURITY-DOG-003-GSD",
+    about: "https://youtu.be/p0INBNLqmPE?si=Y_mhqG0uAbnhCeHg"
+  },
+  {
+    video: "https://drive.google.com/file/d/17men3bzriVQwD70Bl61bGFVcdBYi1yXP/preview",
+    name: "German Sherpherd",
+    age: "1Year 8Months",
+    sex: "Female",
+    location: "Ibadan",
+    price: "300,000",
+    id: "SECURITY-DOG-004-GSD",
+    about: "https://youtu.be/p0INBNLqmPE?si=Y_mhqG0uAbnhCeHg"
+  },
+  {
+    video: "https://drive.google.com/file/d/1hYlMgb-f1kXP98UkkPwuCJJihnKOG5-p/preview",
+    name: "Caucasian Sherpherd",
+    age: "8 Weeks",
+    sex: "Female",
+    location: "jos",
+    price: "650,000",
+    id: "SECURITY-DOG-003-CCS",
+    about: "https://youtu.be/ohy21_csQyc?si=tR_ml0Gvk9t-N-n-"
+  },
+  {
+    video: "https://drive.google.com/file/d/1_OMl1dEQv2hNtwHDjrHakMwrHMFWQAIX/preview",
+    name: "BoerBoel",
+    age: "8 Weeks",
+    sex: "Male",
+    location: "Lagos (Ikorodu)",
+    price: "200,000",
+    id: "SECURITY-DOG-001-BB",
+    about: "https://youtu.be/kBxFOvz7Xao?si=KImxUS3ZkGrHyL7p"
+  },
 ]
